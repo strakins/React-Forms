59b8891175f41bc6d41c532d39a5f625f27112b5
strakins <akinolablessing25@gmail.com>
2022-04-14 09:55:40 +0100

    Small Screen responsiveness adjusted

--- src/component/Forms.jsx
+++ src/component/Forms.jsx
@@ -49,7 +49,7 @@ const Forms = () => {
   return (
     <div className='h-auto mt-4 flex flex-row justify-center p-3'>
         
-        <section className='bg-slate-700 lg:w-1/2 h-auto p-8'>
+        <section className='bg-slate-700 md:w-1/2 h-auto p-8'>
         <div className='bg-slate-500 text-gray-100 h-auto rounded-md shadow-xl mb-4'>
         {
             submitting && 
